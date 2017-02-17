fe2d355850ab5636454f1bf1c1bbf1ce2a81023e
Jonathan Doron <yonidn7@gmail.com>
2017-02-17 14:01:08 +0200

    Added states 1-10

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ let game = new Phaser.Game(600, 400, Phaser.AUTO);
 // Adding a state to our game.
 // add(nameOfState, nameOfProperty in Demo Object)
 game.state.add('state1', demo.state1);
+game.state.add('state2', demo.state2);
+game.state.add('state3', demo.state3);
+game.state.add('state4', demo.state4);
+game.state.add('state5', demo.state5);
+game.state.add('state6', demo.state6);
+game.state.add('state7', demo.state7);
+game.state.add('state8', demo.state8);
+game.state.add('state9', demo.state9);
 
-game.state.start('state1');
+game.state.start('state0');
--- state1.js
+++ state1.js
@@ -1,7 +1,15 @@
+// Game Obejct - only make once:
 let demo = {};
 demo.state1 = function(){};
 demo.state1.prototype = {
+	//  Loading Images
 	preload: function(){},
-	create: function(){},
+	//  Loading background Images
+	create: function(){
+		game.stage.backgroundColor = '#dddddd';
+		console.log("state1");
+	},
+	// Updates frame of the game (60 fps)
+	// Each frame of the game is a still image
 	update: function(){}
 };
