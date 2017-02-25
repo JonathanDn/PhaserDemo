7a54111ca45ac127c7405e839eb75a974935aa8f
Jonathan Doron <yonidn7@gmail.com>
2017-02-25 19:58:21 +0200

    Created volcano with particle system & speech text box that's self generating over itme

--- state6.js
+++ state6.js
@@ -1,9 +1,40 @@
 demo.state6 = function(){};
 demo.state6.prototype = {
-	preload: function(){},
+	preload: function(){
+		game.load.image('volcano', 'assets/sprites/volcano.png');
+		game.load.image('redBall', 'assets/sprites/redBall.png');
+		game.load.image('orBall', 'assets/sprites/orBall.png');
+	},
 	create: function(){
 		game.stage.backgroundColor = '#a6ff4d';
 		addChangeStateEventListners();
+
+		var volcano = game.add.sprite(centerX, 1000, 'volcano').anchor.setTo(0.5, 1);
+
+		// Location of emitter (x, y, particles amount)
+		var emitter = game.add.emitter(centerX, 400, 2000);
+		// particles to emit, 0, number of particles to generate, collision with enemys, collision with world
+		emitter.makeParticles(['redBall', 'orBall'], 0, 5000, false, true);
+		// Horizontal Speed / Vertical Speed
+		emitter.maxParticleSpeed.set(300, -300);
+		emitter.minParticleSpeed.set(-300, -100);
+		// Add gravity to particles:
+		emitter.gravity = 300;
+
+
+		// Time event (happens once) - added loop
+		game.time.events.add(2000, function() {
+			// explode / or last, duration of lasting, frequency of emitting
+			emitter.start(false, 5000, 20);
+
+			game.time.events.loop(500, function(){
+				if (emitter.on){
+					emitter.false;
+				} else {
+					emitter.on = true;
+				}
+			});
+		})
 	},
 	update: function(){}
 };
--- state8.js
+++ state8.js
@@ -1,9 +1,60 @@
+var text;
+
+WebFontConfig = {
+	google: {families: [ 'candal', 'Montserrat']}
+};
+
 demo.state8 = function(){};
 demo.state8.prototype = {
-	preload: function(){},
+	preload: function(){
+		// Google font API script - configurable by WebFontConfig on top
+		game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
+	},
 	create: function(){
 		game.stage.backgroundColor = '#66ffff';
 		addChangeStateEventListners();
+
+		text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
+			'Etiam congue urna et hendrerit bibendum. Donec sed urna enim. ' +
+			'Donec vel orci eu massa pharetra ultricies. Aenean non nunc id ante ' +
+			'ultricies consequat. Aenean dignissim auctor sem, at venenatis risus sagittis nec. ' +
+			'In velit ante, sollicitudin non lacus in, aliquam laoreet sem. Curabitur quis purus ' +
+			'sit amet lacus volutpat ornare quis quis purus. Proin ultricies sem in nisl efficitur, vitae ' +
+			'gravida sem volutpat. Etiam vel convallis eros. Donec semper sem vitae tellus vestibulum.';
+
+		// this.spellOutText(100, 100, 1000, text, 40, 40, '#fff', 'Candal');
+		this.spellOutText(100, 100, 1000, text, 40, 20, '#000', 'Montserrat');
+		// x, y, text say:, font and varios css font properties
+		// game.add.text(100, 400, 'Hello World!');
 	},
-	update: function(){}
+	update: function(){},
+
+	spellOutText: function(x, y, width, text, fontSize, speed, fill, font) {
+		var sentence = game.add.text(x, y, '', {fontSize: fontSize + 'px', fill: fill, font: font});
+		var currentLine = game.add.text(10, 10, '', {fontSize: fontSize + 'px', font: font});
+		currentLine.alpha = 0;
+		var loop = game.time.events.loop(speed, addChar);
+
+		var index = 0;
+
+		function addChar() {
+			sentence.text += text[index];
+			currentLine.text += text[index];
+
+			// compares invisible line to text line to know when to keep writing to an other row
+			// && condition - makes sure a word would not be cut in the middle
+			if(currentLine.width > width && text[index] === ' ') {
+				sentence.text += '\n';
+				currentLine.text = '';
+			}
+			// when text is finished stop looping
+			if(index >= text.length -1) {
+				game.time.events.remove(loop);
+				console.log('stop')
+			}
+
+			index++;
+		}
+
+	}
 };
